cc1d672eaff917f5a2fb7b34fff09408215acf28
Daniel S. Costa <daniel.sousa@msn.com>
2021-05-26 15:07:47 -0300

    adicionado menu mobile

---
+++ src/js/menu.js
@@ -0,0 +1,8 @@
+Menu = {
+    toggleMenu() {
+        document.querySelector(".header-menu > ul").classList.toggle("active");
+    },
+    toggleSubmenu() {
+        document.querySelector(".sub-menu > ul").classList.toggle("active");
+    },
+};
